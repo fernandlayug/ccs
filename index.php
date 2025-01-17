1e548690fcb38ee668dbcb85107c586a17554ad5
DESKTOP-EMLQ49Q\Ian PC <garciaiancarlo1@gmail.com>
2025-01-16 22:30:57 -0800

    add ccs logo, ssite, class offers

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ try {
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
         <a class="navbar-brand text-white" href="#">
-            <img src="pic/Ccs.png" alt="CCS Logo" style="width: 70px; height: auto;"> College of Computer Studies
+            <img src="pic/ccs-logo.png" alt="CCS Logo" style="width: 40px; height: auto;"> College of Computer Studies
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
