e827242cd0e7bb0420e8e95638f8da6131f23bf5
DESKTOP-EMLQ49Q\Ian PC <garciaiancarlo1@gmail.com>
2025-01-20 15:34:33 -0800

    static/css/index.css

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>College of Computer Studies</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="static/css/index.css">
 </head>
 <body>
 
